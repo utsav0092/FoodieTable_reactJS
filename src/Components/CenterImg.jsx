4fef7a463119846b79ced8c3e4d675d375f1e96e
Utsav Chouhan <144726026+utsav0092@users.noreply.github.com>
2025-12-25 23:33:03 +0530

    Revert "Make banner image responsive across different devices with adaptive height breakpoints"
    This reverts commit 7ed30944476b12b1637af0e84937a76e125dee58.

--- src/Components/CenterImg.jsx
+++ src/Components/CenterImg.jsx
@@ -3,12 +3,8 @@ import centerImg from '../assets/centerImg.jpg';
 
 const CenterImg = () => {
     return (
-        <div className='w-full overflow-hidden'>
-            <img 
-                src={centerImg} 
-                alt="Image banner" 
-                className='h-[40vh] sm:h-[50vh] md:h-[60vh] lg:h-[70vh] w-full object-cover object-center' 
-            />
+        <div>
+            <img src={centerImg} alt="Image banner" className='h-[60vh] w-full' />
         </div>
     );
 }
